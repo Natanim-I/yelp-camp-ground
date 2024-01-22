1c5ad2b8ab632afad423ad26d07221821f07fa8b
Natanim Issa <natanimissa@gmail.com>
2024-01-22 12:50:17 -0500

    added error and success flash messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,28 +27,39 @@ router.get("/new", (req, res) => {
 router.post("/", validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground)
     await campground.save()
+    req.flash("success", "Campground successfully created!")
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 router.get("/:id", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate("reviews")
+    if(!campground){
+        req.flash("error", "Can not find that campground!")
+        return res.redirect("/campgrounds")
+    }
     res.render("campgrounds/show", {campground}) 
 }))
 
 router.get("/:id/edit", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if(!campground){
+        req.flash("error", "Can not find that campground!")
+        return res.redirect("/campgrounds")
+    }
     res.render("campgrounds/edit", {campground}) 
 }))
 
 router.put("/:id", validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params
     const updatedCampground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, {runValidators: true})
+    req.flash("success", "Campground successfully updated!")
     res.redirect(`/campgrounds/${updatedCampground._id}`)
 }))
 
 router.delete("/:id", catchAsync(async (req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Campground successfully deleted!")
     res.redirect("/campgrounds")
 }))
 
